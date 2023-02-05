4329a53fa9a567fa5aca59526d94fb4d12cc1655
YurijU <2132410@mail.ru>
2023-02-05 12:57:44 +0700

    Added others methods.

--- 3-calc/index.js
+++ 3-calc/index.js
@@ -1,10 +1,14 @@
 // node index.js num num <operation>
 const { add } = require('./add.js');
 const { divide } = require('./divide.js');
+const { minus } = require('./minus.js');
+const { multiply } = require('./multiply.js');
+
 
 let firstNum = Number(process.argv[2]);
 let secondNum = Number(process.argv[3]);
 let operation = process.argv[4];
+
 
 if ( isNaN(firstNum) || isNaN(secondNum)) {
     console.log('Operation parameter is not a number. Try again.');
@@ -23,10 +27,10 @@ if ( isNaN(firstNum) || isNaN(secondNum)) {
             res = divide(firstNum, secondNum);
             break;
         case('minus'):
-            //
+            res = minus(firstNum, secondNum);
             break;
         case('multiply'):
-            //
+            res = multiply(firstNum, secondNum);
             break;
         default:
             console.log(`Operation ${operation} not implemented.`);
